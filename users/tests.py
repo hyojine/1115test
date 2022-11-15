dcf524a33b52012180ce780b8d77aa0a8918a176
hyojine <touroz@naver.com>
2022-11-15 09:22:44 +0900

    1115test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class UserRegistrationTest(APITestCase):
+    def test_registration(self):
+        url = reverse('userview')
+        user_data = {
+            "username":"test",
+            "fullname":"테스터",
+            "email":"test@testuser.com",
+            "password":"password",
+        }
+        response = self.client.post(url,user_data)
+        self.assertEqual(response.data['message'],"가입 완료!!")
--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     # user/
-    path('', views.UserView.as_view()),
+    path('', views.UserView.as_view(),name='userview'),
     path('api/token/', views.SpartaTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
  
